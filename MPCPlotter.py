a53f36013c0d55dbbcd9b29731a0ae887ed3953b
sihengt <siheng.teng@gmail.com>
2025-05-21 16:01:34 -0400

    script for figuring out how to drive Clifford

---
+++ MPCPlotter.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class MPCPlotter:
+    def __init__(self, track, sim_duration):
+        # Initialize figure and create a grid with 2 rows, 3 columns
+        self.fig = plt.figure(figsize=(10, 8))
+        gs = self.fig.add_gridspec(2, 3)
+
+        self.ax_track = self.fig.add_subplot(gs[0, :])
+        self.ax_track.axis("equal")
+        self.ax_track.set_xlabel("x (m)")
+        self.ax_track.set_ylabel("y (m)")
+        
+        self.ax_acc = self.fig.add_subplot(gs[1, 0])
+        self.ax_acc.set_xlabel("Time")
+        self.ax_acc.set_ylabel("Acceleration (m/s^2)")
+        
+        self.ax_df = self.fig.add_subplot(gs[1, 1])
+        self.ax_df.set_ylabel("Front Steering (radians)")
+        self.ax_df.set_xlabel("Time")
+        
+        self.ax_dr = self.fig.add_subplot(gs[1, 2])
+        self.ax_dr.set_xlabel("Time")
+        self.ax_dr.set_ylabel("Rear Steering (radians)")
+
+        if len(track) != 0:
+            self.track = track
+            self.plot_track(track)
+
+        # For storing incoming data
+        self.X = np.zeros((4, sim_duration))
+        self.U = np.zeros((3, sim_duration))
+        
+        self.current_timestep = 0
+    
+    def plot_track(self, track):
+        self.ax_track.plot(track[0, :], track[1, :], "b", label="Track")
+
+    def plot_new_data(self, c_state, actions, nn_idx, T, i_timestep):
+        """
+        Params:
+            c_state: cartesian state (x, y) of where robot currently is
+            actions: only accepts actions of following form (acceleration, front steering, rear steering)
+        """
+        # Updates incoming data
+        self.X[:, i_timestep] = c_state
+        self.U[:, i_timestep] = actions
+        
+        N_timesteps = np.arange(i_timestep)
+
+        self.ax_track.clear()
+        self.plot_track(self.track)
+        
+        # TODO: problematic
+        horizon = self.track[:, nn_idx:nn_idx+T]
+        
+        self.ax_track.scatter(horizon[0, :], horizon[1, :], s=0.8, color='orange', zorder=1)
+        self.ax_track.scatter(self.X[0, :i_timestep], self.X[1, :i_timestep], s=0.5, color='red', zorder=0.5)
+        self.ax_track.plot(self.X[0, :i_timestep], self.X[1, :i_timestep], color='green', zorder=-1)
+
+        self.ax_acc.clear()
+        self.ax_acc.plot(N_timesteps, self.U[0, :i_timestep])
+
+        self.ax_df.clear()
+        self.ax_df.plot(N_timesteps, self.U[1, :i_timestep])
+
+        self.ax_dr.clear()
+        self.ax_dr.plot(N_timesteps, self.U[2, :i_timestep])
+
+    def plot_new_data_without_track(self, c_state, actions, i_timestep):
+        """
+        Params:
+            c_state: cartesian state (x, y) of where robot currently is
+            actions: only accepts actions of following form (acceleration, front steering, rear steering)
+        """
+        # Updates incoming data
+        self.X[:, i_timestep] = c_state
+        self.U[:, i_timestep] = actions
+        
+        N_timesteps = np.arange(i_timestep)
+
+        self.ax_track.scatter(self.X[0, :i_timestep], self.X[1, :i_timestep], s=0.5, color='red', zorder=0.5)
+        self.ax_track.plot(self.X[0, :i_timestep], self.X[1, :i_timestep], color='green', zorder=-1)
+
+        self.ax_acc.clear()
+        self.ax_acc.plot(N_timesteps, self.U[0, :i_timestep])
+
+        self.ax_df.clear()
+        self.ax_df.plot(N_timesteps, self.U[1, :i_timestep])
+
+        self.ax_dr.clear()
+        self.ax_dr.plot(N_timesteps, self.U[2, :i_timestep])
+
+    def save_plot(self, filename, dpi=300, bbox_inches='tight'):
+        self.fig.savefig(filename, dpi=dpi, bbox_inches=bbox_inches)
---
+++ driving.py
@@ -0,0 +1,135 @@
+import yaml
+import os
+import logging
+import pybullet as p
+from utils.planarRobotState import convert_planar
+from utils.planarRobotState import convert_planar_world_frame
+from clifford_pybullet.CliffordRobot import CliffordRobot
+from clifford_pybullet.RandomRockyTerrain import RandomRockyTerrain
+from clifford_pybullet.Terrain import Terrain
+from clifford_pybullet.SimController import SimController
+from clifford_pybullet.utils.genParam import genParam
+
+import torch
+import numpy as np
+from control.PurePursuit import PurePursuit
+from plotting.PurePursuitPlot import PurePursuitPlot
+import matplotlib.pyplot as plt
+
+from TrajProc.TrajProc import TrajProc
+from TrajProc.models.DSKBM import csDSKBM
+from TrajProc.controls.MPC import MPC
+
+from MPCPlotter import MPCPlotter
+
+# from TrajProc.scripts import *
+import casadi as cs
+import time
+
+from simple_pid import PID
+
+############
+## FOR MPC #
+############
+N_STATES = 4
+N_ACTIONS = 3
+L = 0.684
+l_f = 0.342
+l_r = 0.342
+T = 10
+DT = 0.1 # "simTimeStep": 0.004166666666666 * number of steps per control: 24
+SIM_DURATION = 200  # time steps
+
+MAX_SPEED = 10.0
+MAX_STEER = np.radians(30)
+MAX_D_ACC = 2.0
+MAX_D_STEER = np.radians(30)  # rad/s
+MAX_ACC = 5.0
+REF_VEL = 1.0
+
+def main(data_dir):    
+    params = {
+        'dataDir':      data_dir,
+        'controls':     yaml.safe_load(open(os.path.join(data_dir,'config/controls.yaml'),'r')),
+        'robotRange':   yaml.safe_load(open(os.path.join(data_dir,'config/robotRange.yaml'),'r')),
+        'sim':          yaml.safe_load(open(os.path.join(data_dir,'config/sim.yaml'),'r')),
+        'terrain':      yaml.safe_load(open(os.path.join(data_dir,'config/terrain.yaml'),'r')),
+        'train':        yaml.safe_load(open(os.path.join(data_dir,'config/train.yaml'),'r')),
+        }
+    
+    # plt.ion()
+    tp = TrajProc()
+
+    # PyBullet + robot setup
+    physicsClientID = p.connect(p.GUI)
+    # physicsClientID = p.connect(p.DIRECT)
+    robotParams = genParam(params['robotRange'], gen_mean=params['train']['useNominal'])
+    robot = CliffordRobot(robotParams)
+    
+    # Terrain
+    terrain = Terrain(params['terrain'], physicsClientId=physicsClientID)
+
+    targetVelocitySlider = p.addUserDebugParameter("wheelVelocity", -8, 8, 4.0)
+    maxForceSlider = p.addUserDebugParameter("maxForce", 0, 10.0, 2.5)
+
+    sim = SimController(
+        robot,
+        terrain,
+        params['sim'],
+        physicsClientId=physicsClientID,
+        realtime=True,
+        camFollowBot=True,
+        stateProcessor=convert_planar_world_frame
+    )
+    
+    sim.resetRobot(pose=((0,0),(0, 0, 0, np.sqrt(2)/2)))
+
+    mp = MPCPlotter([], SIM_DURATION)
+    plt.pause(0.0001)
+
+    # VARIABLES FOR TRACKING
+    x_sim = np.zeros((N_STATES, SIM_DURATION))
+    u_sim = np.zeros((N_ACTIONS, SIM_DURATION - 1))
+
+    # Step 2: Create starting conditions x0
+    x_sim[:, 0] = np.array([0.0, 0.0, 0.0, 0.0]).T
+    
+    # Create and configure logger
+    logging.basicConfig(
+        filename="logs/drivestate.log",
+        format='%(asctime)s %(message)s',
+        filemode='w',
+        level=logging.INFO)
+    state_logger = logging.getLogger('States')
+    
+    for sim_time in range(SIM_DURATION - 1):
+        targetVelocity = p.readUserDebugParameter(targetVelocitySlider)
+        maxForce = p.readUserDebugParameter(maxForceSlider)
+
+        plt.pause(0.0001)
+        iter_start = time.time()
+
+        # Wheel velocity of 5.0 (vehicular) / 0.1 (radius)
+        u_sim[:, sim_time] = np.array([targetVelocity / 0.1, 0, 0])
+
+        current_state = x_sim[:, sim_time]
+        mp.plot_new_data_without_track(current_state, u_sim[:, sim_time], sim_time)
+                
+        with open("log_actions.txt", "a") as f:
+            f.write("Sim_time={}\t u_sim={}\n".format(sim_time, u_sim[:, sim_time]))
+        
+        # TODO: add check for termFlag.
+        # Sends action to simulator to drive the robot by "simTimeStep" "numStepsPerControl" times.
+        lastState, action, current_state, termFlag = sim.controlLoopStep(torch.tensor(u_sim[:, sim_time]))
+
+        # current_state: [x, y, heading, vel_x, vel_y, vel_theta]
+        current_state = current_state.numpy()
+        current_state[[2, 3]] = current_state[[3, 2]]
+        x_sim[:, sim_time + 1] = current_state[:4]
+
+        state_logger.info(x_sim[:, sim_time + 1])
+    
+    breakpoint()
+
+if __name__ == "__main__":
+    main("nominal_dec_8")
