9bacefb6de25ef6d5033f1062b2d42cc990f66bb
sihengt <siheng.teng@gmail.com>
2025-05-23 22:40:51 -0400

    Major cleanup
    - Moved most function blocks to simulation_utils
    - main is clean
    - tried multiprocessing but realized it doesn't make sense

--- main.py
+++ main.py
@@ -1,99 +1,38 @@
-import yaml
-import os
-import logging
 import pybullet as p
-
-from utils.planarRobotState import convert_planar_world_frame
-from clifford_pybullet.CliffordRobot import CliffordRobot
-from clifford_pybullet.Terrain import Terrain
-from clifford_pybullet.SimController import SimController
-from clifford_pybullet.utils.genParam import genParam
-
 import numpy as np
 import matplotlib.pyplot as plt
+import yaml
+import os
+
+from utils.simulation.simulation_utils import init_u_bar, \
+    init_logger, plan_mpc_step, step_sim_and_log, create_debug_track, create_random_track, init_pybullet_sim, mpc_worker
 
 from TrajProc.TrajProc import TrajProc
 from TrajProc.models.DSKBM import csDSKBM
 from TrajProc.controls.MPC import MPC
 
 from MPCPlotter import MPCPlotter
-from MPCConfigLoader import MPCConfigLoader
 
-import casadi as cs
-import time
-
-from simple_pid import PID
-
+import multiprocessing as mp
 
 # TODO: YAMLify this.
 SIM_DURATION = 200  # time steps ## NOT IN YAML
 
 def main(data_dir):    
-    params = {
-        'dataDir':      data_dir,
-        'controls':     yaml.safe_load(open(os.path.join(data_dir,'config/controls.yaml'),'r')),
-        'robotRange':   yaml.safe_load(open(os.path.join(data_dir,'config/robotRange.yaml'),'r')),
-        'sim':          yaml.safe_load(open(os.path.join(data_dir,'config/sim.yaml'),'r')),
-        'terrain':      yaml.safe_load(open(os.path.join(data_dir,'config/terrain.yaml'),'r')),
-        'train':        yaml.safe_load(open(os.path.join(data_dir,'config/train.yaml'),'r')),
-        }
-    
-    # Handling parameters for MPC
-    mpc_config_loader = MPCConfigLoader(os.path.join(data_dir,'config/mpc.yaml'))
-    # Converting parameters from list form to casadi parameters usable by MPC class
-    mpc_params = mpc_config_loader.construct_casadi_params()
-    
     plt.ion()
-    tp = TrajProc()
-
+    
     # PyBullet + robot setup
-    # physicsClientID = p.connect(p.GUI)
-    physicsClientID = p.connect(p.DIRECT)
-    robotParams = genParam(params['robotRange'], gen_mean=params['train']['useNominal'])
-    robot = CliffordRobot(robotParams)
-    
-    # Terrain
-    terrain = Terrain(params['terrain'], physicsClientId=physicsClientID)
-    # terrain = RandomRockyTerrain(params['terrain'], physicsClientId=physicsClientID)
-    # terrain.generate()
-    
-    sim = SimController(
-        robot,
-        terrain,
-        params['sim'],
-        physicsClientId=physicsClientID,
-        realtime=True,
-        camFollowBot=True,
-        stateProcessor=convert_planar_world_frame
-    )
+    params, mpc_params, sim = init_pybullet_sim(data_dir)
     
     sim.resetRobot(pose=((0,0),(0, 0, 0, np.sqrt(2)/2)))
-
+    
+    tp = TrajProc()
     cs_kbm = csDSKBM(mpc_params)
 
     # Creating a sample trajectory to track
-    xs = np.array([0, 125, 125, -125, -180, -75, 0]) * params['terrain']['mapScale']
-    ys = np.array([0, 125, -125, -125, 75, 100, 0]) * params['terrain']['mapScale']
-    track = tp.generate_path_from_wp(
-        xs,
-        ys,
-        0.05
-    )
+    track = create_debug_track(tp, params['terrain'])
 
-    mp = MPCPlotter(track, SIM_DURATION)
-
-    # [DEBUG] adds track into PyBullet
-    # Colors start black and eventually turn white.
-    colors = np.linspace(0, 1, xs.shape[0]-1)
-    for i in range(xs.shape[0] - 1):
-        p.addUserDebugLine(
-            np.array([xs[i], ys[i], 0.1]),
-            np.array([xs[i+1], ys[i+1], 0.1]),
-            [colors[i], colors[i], colors[i]],
-            5.0
-        )
-    
-    opt_time = []
+    mpc_plotter = MPCPlotter(track, SIM_DURATION)
 
     # VARIABLES FOR TRACKING
     x_sim = np.zeros((mpc_params['model']['nStates'], SIM_DURATION))
@@ -103,117 +42,25 @@ def main(data_dir):
     x_sim[:, 0] = np.array([0.0, 0.0, 0.0, 0.0]).T
 
     # Step 3: Generate starting guess for u_bar (does not have to be too accurate I suppose.)
-    u_bar_start = np.zeros((mpc_params['model']['nActions'], mpc_params['T']))
-    u_bar_start[0, :] = mpc_params['maxAcc'] / 2
-    u_bar_start[1, :] = 0.0
-    u_bar_start[2, :] = 0.0
-
-    l_a = []
-    l_df = []
-    l_dr = []
-    l_state = []
+    u_bar_start = init_u_bar(mpc_params)
 
     mpc = MPC(mpc_params, cs_kbm)
     
-    # Create and configure logger
-    logging.basicConfig(
-        filename="logs/state.log",
-        format='%(asctime)s %(message)s',
-        filemode='w',
-        level=logging.INFO)
-    state_logger = logging.getLogger('States')
+    state_logger = init_logger()
     
+    ## MULTIPROCESSING (try)
     for sim_time in range(SIM_DURATION - 1):
-        iter_start = time.time()
-    
-        x_ref, nn_idx, l_ref_idx = tp.get_reference_trajectory(
-            x_sim[:, sim_time],
-            track,
-            mpc_params['refVel'],
-            0.05,
-            mpc_params['T'],
-            mpc_params['dt']
-        )
-        
-        # TO DEBUG, plot points of nn_idx to T
-        # [DEBUG] Trying to see if we reach the end of path tracking
-        if track.shape[1] - nn_idx < 0.01 * 1115:
-            print("we're close")
-            print(nn_idx)
+        # Solves QP and returns optimal action in u_sim_opt (converted from MPC to sim action)
+        u_sim_opt, l_ref_idx = plan_mpc_step(x_sim[:, sim_time], u_bar_start, track, tp, mpc, mpc_params)
+                
+        # Store action to be taken this sim_time into u_sim
+        u_sim[:, sim_time] = u_sim_opt.numpy()
 
-        if nn_idx == track.shape[1]:
-            print("Reached.")
-            break
-        
-        # You can add a warm-start to u_bar through u_bar_start.
-        # In this loop we use the previous solved controls as a warmstart.
-        X_mpc, U_mpc, x_ref = mpc.predict(x_sim[:, sim_time], x_ref, u_bar_start)
-            
-        a_mpc   = np.array(U_mpc[0, :]).flatten()
-        d_f_mpc = np.array(U_mpc[1, :]).flatten()
-        d_r_mpc = np.array(U_mpc[2, :]).flatten()
-        u_bar = np.vstack(((X_mpc[2, 0] + a_mpc[0] * mpc_params['dt']) / 0.1, d_f_mpc[0], d_r_mpc[0]))
-
-        current_state = X_mpc[:, 0]
-        l_state.append(current_state)
-
-        curr_a = U_mpc[0, 0]
-        curr_df = U_mpc[1, 0]
-        curr_dr = U_mpc[2, 0]
-        
-        # Wheel velocity = omega = v_com / wheel_radius.
-        # TODO: I've hard-coded wheel radius to be 0.1 here.
-        l_a.append([(X_mpc[2, 0] + curr_a * mpc_params['dt']) / 0.1])
-        l_df.append(curr_df)
-        l_dr.append(curr_dr)
-        
-        # Take first action
-        u_sim[:, sim_time] = u_bar[:, 0]
-
-        current_state = x_sim[:, sim_time]
-        mp.plot_new_data(current_state, u_sim[:, sim_time], l_ref_idx, sim_time)
-        
-        # print("curr_v:{}\t dt:{}\t curr_a:{}".format(X_mpc[2,0], DT, curr_a))
-        # print("Action to take:{}".format(torch.tensor(u_sim[:, sim_time])))
-        print("nn_idx={}/{}".format(nn_idx, track.shape[1]))
-        time_taken = time.time() - iter_start
-        print("Time taken for solver:{}".format(time_taken))
-        
-        # Save actions solved by MPC as warm-start for next iteration.
-        # u_bar_start = u_bar
-        
-        with open("log_actions.txt", "a") as f:
-            f.write("Sim_time={}\t u_sim={}\n".format(sim_time, u_sim[:, sim_time]))
-        
-        # TODO: add check for termFlag.
-        # Sends action to simulator to drive the robot by "simTimeStep" "numStepsPerControl" times.
-        lastState, action, current_state, termFlag = sim.controlLoopStep(u_sim[:, sim_time], commandInRealUnits=True)
-
-        # current_state: [x, y, heading, vel_x, vel_y, vel_theta]
-        current_state = current_state.numpy()
-        current_state[[2, 3]] = current_state[[3, 2]]
-        print("curr_state:{}".format(current_state))
-        x_sim[:, sim_time + 1] = current_state[:4]
-
-        state_logger.info(x_sim[:, sim_time + 1])
-
-        # Measure elapsed time for MPC
-        opt_time.append(time_taken)
-        
+        # Update plots
+        mp.plot_new_data(x_sim[:, sim_time], u_sim[:, sim_time], l_ref_idx, sim_time)
         plt.pause(0.0001)
-    
-    import pickle
-    with open("opt_time.pkl", "wb") as pkl:
-        pickle.dump(opt_time, pkl)
-    with open("l_state.pkl", "wb") as pkl:
-        pickle.dump(l_state, pkl)
-    with open("l_a.pkl", "wb") as pkl:
-        pickle.dump(l_a, pkl)
-    with open("l_df.pkl", "wb") as pkl:
-        pickle.dump(l_df, pkl)
-    with open("l_dr.pkl", "wb") as pkl:
-        pickle.dump(l_dr, pkl)
-
+                
+        step_sim_and_log(sim, sim_time, x_sim, u_sim, state_logger)
 
 if __name__ == "__main__":
     main("nominal_dec_8")
---
+++ utils/simulation/simulation_utils.py
@@ -0,0 +1,198 @@
+import numpy as np
+import logging
+import pybullet as p
+from TrajProc.controls.translator import MpcAction, MpcState, SimState, MpcToSimAction
+from clifford_pybullet.CliffordRobot import CliffordRobot
+from clifford_pybullet.Terrain import Terrain
+from clifford_pybullet.SimController import SimController
+from clifford_pybullet.utils.genParam import genParam
+from utils.planarRobotState import convert_planar_world_frame
+from MPCConfigLoader import MPCConfigLoader
+
+from TrajProc.TrajProc import TrajProc
+from TrajProc.models.DSKBM import csDSKBM
+from TrajProc.controls.MPC import MPC
+
+import yaml
+import os
+
+def init_pybullet_sim(data_dir):
+    params = {
+        'dataDir':      data_dir,
+        'controls':     yaml.safe_load(open(os.path.join(data_dir,'config/controls.yaml'),'r')),
+        'robotRange':   yaml.safe_load(open(os.path.join(data_dir,'config/robotRange.yaml'),'r')),
+        'sim':          yaml.safe_load(open(os.path.join(data_dir,'config/sim.yaml'),'r')),
+        'terrain':      yaml.safe_load(open(os.path.join(data_dir,'config/terrain.yaml'),'r')),
+        'train':        yaml.safe_load(open(os.path.join(data_dir,'config/train.yaml'),'r')),
+        }
+
+    # Converting parameters from list form to casadi parameters usable by MPC class
+    mpc_config_loader = MPCConfigLoader(os.path.join(data_dir,'config/mpc.yaml'))
+    mpc_params = mpc_config_loader.construct_casadi_params()
+
+    # YOU'RE LOOKING FOR THIS!
+    # physicsClientID = p.connect(p.DIRECT)
+    physicsClientID = p.connect(p.GUI)
+
+    robotParams = genParam(params['robotRange'], gen_mean=params['train']['useNominal'])
+    robot = CliffordRobot(robotParams)
+
+    # Terrain
+    terrain = Terrain(params['terrain'], physicsClientId=physicsClientID)
+
+    sim = SimController(
+        robot,
+        terrain,
+        params['sim'],
+        physicsClientId=physicsClientID,
+        realtime=True,
+        camFollowBot=True,
+        stateProcessor=convert_planar_world_frame
+    )
+
+    return params, mpc_params, sim
+
+def create_debug_track(tp, terrain_params, debug=True):
+    # Creating a sample trajectory to track
+    xs = np.array([0, 125, 125, -125, -180, -75, 0]) * terrain_params['mapScale']
+    ys = np.array([0, 125, -125, -125, 75, 100, 0])  * terrain_params['mapScale']
+    track = tp.generate_path_from_wp(
+        xs,
+        ys,
+        0.05
+    )
+
+    if debug:
+        draw_debug_track(xs, ys)
+
+    return track
+
+def create_random_track(tp, terrain_params, n_vertices, debug=True):
+    x_max = terrain_params['mapWidth'] / 2
+    x_min = -x_max
+    y_max = terrain_params['mapLength'] / 2
+    y_min = -y_max
+    
+    xs = np.concatenate( ([0], np.random.randint(x_min, x_max, n_vertices - 1)) ) * terrain_params['mapScale']
+    ys = np.concatenate( ([0], np.random.randint(y_min, y_max, n_vertices - 1)) ) * terrain_params['mapScale']
+
+    track = tp.generate_path_from_wp(
+        xs,
+        ys,
+        0.05
+    )
+
+    if debug:
+        draw_debug_track(xs, ys)    
+
+    return track
+
+def draw_debug_track(xs, ys):
+    # [DEBUG] adds track into PyBullet
+    # Colors start black and eventually turn white.
+    colors = np.linspace(0, 1, xs.shape[0]-1)
+    for i in range(xs.shape[0] - 1):
+        p.addUserDebugLine(
+            np.array([xs[i], ys[i], 0.1]),
+            np.array([xs[i+1], ys[i+1], 0.1]),
+            [colors[i], colors[i], colors[i]],
+            5.0
+        )    
+    return
+
+def init_logger():
+    # Create and configure logger
+    logging.basicConfig(
+        filename="logs/state.log",
+        format='%(asctime)s %(message)s',
+        filemode='w',
+        level=logging.INFO)
+    state_logger = logging.getLogger('States')
+    return state_logger
+
+def init_u_bar(mpc_params):
+    u_bar_start = np.zeros((mpc_params['model']['nActions'], mpc_params['T']))
+    u_bar_start[0, :] = mpc_params['maxAcc'] / 2
+    u_bar_start[1, :] = 0.0
+    u_bar_start[2, :] = 0.0
+
+    return u_bar_start
+
+def plan_mpc_step(current_state, u_bar_start, ref_track, tp, mpc, mpc_params):
+    """
+    Params:
+        current_state:  state of the following form [x, y, velocity, yaw]
+        u_bar_state:    reference actions that repeat T times [[acc, left_steering, right_steering], ...]
+        tp:             Trajectory Processor class that contains functions for processing trajectories
+        mpc:            MPC class
+        mpc_params:     parameters used in optimization
+
+    Returns:
+
+    """
+    # TODO: add a check for whether we are close to the end. If so, either amend get_reference_trajectory OR the 
+    # states or do something.
+    x_ref, nn_idx, l_ref_idx = tp.get_reference_trajectory(
+        current_state,
+        ref_track,
+        mpc_params['refVel'],
+        0.05,
+        mpc_params['T'],
+        mpc_params['dt']
+    )
+
+    # You can add a warm-start to u_bar through u_bar_start.
+    # In this loop we use the previous solved controls as a warmstart.
+    X_mpc, U_mpc, x_ref = mpc.predict(current_state, x_ref, u_bar_start)
+
+    # Converting action from MPC-space to sim space
+    x_mpc = MpcState(*X_mpc[:, 0])
+    u_mpc = MpcAction(*U_mpc[:, 0])
+    u_sim_opt = MpcToSimAction(0.1, mpc_params['dt'], x_mpc.com_v, u_mpc)
+
+    return u_sim_opt, l_ref_idx
+
+def mpc_worker(state_q, result_q, stop_evt, mpc_params, ref_track):
+    """
+    Params:
+        current_state:  state of the following form [x, y, velocity, yaw]
+        u_bar_state:    reference actions that repeat T times [[acc, left_steering, right_steering], ...]
+        tp:             Trajectory Processor class that contains functions for processing trajectories
+        mpc:            MPC class
+        mpc_params:     parameters used in optimization
+
+    Returns:
+
+    """
+    tp = TrajProc()
+    cs_kbm = csDSKBM(mpc_params)
+    mpc = MPC(mpc_params, cs_kbm)
+
+    u_bar_start = init_u_bar(mpc_params)
+
+    while not stop_evt.is_set():
+        current_state = state_q.get()
+        if current_state is None:
+            break
+
+        u_sim_opt, l_ref_idx = plan_mpc_step(
+            current_state, u_bar_start, ref_track, tp, mpc, mpc_params
+        )
+
+        result_q.put((u_sim_opt, l_ref_idx))
+
+
+def step_sim_and_log(sim, sim_time, x_sim, u_sim, state_logger):
+    _, _, current_state, termFlag = sim.controlLoopStep(u_sim[:, sim_time], commandInRealUnits=True)
+    
+    # We have to swap heading and vel_body for MPC conventions
+    # current_state: [x, y, heading, vel_body]
+    current_state = current_state.numpy()
+    current_state[[2, 3]] = current_state[[3, 2]]
+    x_sim[:, sim_time + 1] = current_state
+    state_logger.info(x_sim[:, sim_time + 1])
+
+    pass
+
+def do_stuff(x_sim):
+    return
